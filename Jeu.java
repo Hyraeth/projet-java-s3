cd0ef09b3da7206f09882ce41457d34d35cdecd4
Yannis Didelot <yannis.didelot@free.fr>
2019-12-31 00:33:52 +0100

    constructeur Jeu.java (pas sur, a verifier)

--- Jeu.java
+++ Jeu.java
@@ -10,4 +10,17 @@ public class Jeu {
     private Centre centre;
     private ArrayList<Joueur> joueurs;
     
+    public Jeu(ArrayList<String> noms) {
+        this.sac = new Sac();
+        this.defausse = new Defausse();
+        this.fabriques = new Fabrique[(noms.size())*2+1];
+        for (int i = 0; i < fabriques.length; i++) {
+            this.fabriques[i] = new Fabrique();
+        }
+        this.centre = new Centre();
+        this.joueurs = new ArrayList<Joueur>();
+        for (String nom : noms) {
+            this.joueurs.add(new Joueur(nom));
+        }
+    }
 }
