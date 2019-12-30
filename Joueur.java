f2bd0271a307a70d328d76f5212225cc2e52eea0
Yannis Didelot <yannis.didelot@free.fr>
2019-12-30 23:28:18 +0100

    constructeur pour Joueur

--- Joueur.java
+++ Joueur.java
@@ -4,4 +4,10 @@ public class Joueur {
     private int score;
     private String nom;
     private Plateau plateau;
+
+    public Joueur (String n) {
+        nom = n;
+        score = 0;
+        plateau = new Plateau();
+    }
 }
