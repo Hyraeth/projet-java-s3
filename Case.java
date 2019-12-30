08225438fe9d7b9e2e3da2a337a118eb6a6bb82d
Yannis Didelot <yannis.didelot@free.fr>
2019-12-30 22:26:12 +0100

    Upload Case.java

---
+++ Case.java
@@ -0,0 +1,25 @@
+package Project;
+
+public class Case {
+
+    private Tuile tuile;
+    public Tuile getTuile() { return this.tuile; }
+
+    public Case() {
+        this.tuile = null;
+    }
+
+    public void addTuile(Tuile t) {
+        this.tuile = t;
+    }
+
+    public void clear() {
+        tuile = null;
+    }
+
+    public boolean isEmpty() {
+        if(tuile == null) return true;
+        return false;
+    }
+
+}
