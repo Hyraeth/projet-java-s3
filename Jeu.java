6ccc5014bf13f7c05ac8450879cc674199f5fb9a
Yannis Didelot <yannis.didelot@free.fr>
2019-12-30 22:23:44 +0100

    Upload Jeu.java

---
+++ Jeu.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class Jeu {
+
+    private Sac sac;
+    private Defausse defausse;
+    private Fabrique[] fabriques;
+    private Centre centre;
+    private Joueur[] joueurs;
+    
+}
